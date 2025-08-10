58d50af449f6ebdaf3126fc68021ce9a9b9901d0
bhuvan <bhuvanannappa@gmail.com>
2025-08-10 23:40:36 +0530

    Created user model

---
+++ models/user-model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "moderator"],
+    required: true,
+    default: "user",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
