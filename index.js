5ded8e0b83f8236f37c8b2f95f8b65dc20ab97d1
bhuvan <bhuvanannappa@gmail.com>
2025-08-10 22:20:00 +0530

    Completed navbar

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.render("home");
 });
 
 app.listen(3000, () => {
